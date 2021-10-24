aa226a06c3b1254e28d52cbb2b77a4d443fa6e26
whitebum <86977637+whitebum@users.noreply.github.com>
2021-10-24 18:45:41 +0900

    Add files via upload
    진짜 마지막 패치 더 이상은 안할듯

---
+++ English_Workbook/English_Workbook/Singleton.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+class Singleton
+{
+protected:
+
+	Singleton() {}
+	Singleton(const Singleton&) = delete;
+	void operator = (const Singleton&) = delete;
+
+public:
+
+	static T& GetInstance()
+	{
+		static T* instance = new T();
+		return *instance;
+	}
+
+};
